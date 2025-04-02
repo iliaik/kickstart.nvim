6629f8205f1719ff56b87458ea11c40f564801ce
Ilia Kosharov <kosharov.ilya@gmail.com>
2025-04-02 12:41:56 +0900

    zen-mode window width setting to 100 (106 to match)

--- init.lua
+++ init.lua
@@ -907,6 +907,9 @@ require('lazy').setup({
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
+        window = {
+                width = 106,
+            },
       }
 
     },
